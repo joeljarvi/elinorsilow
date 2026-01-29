2cd11ff613c06c1a13026cce2266ada764e65e8f
joeljarvi <joeljarvi@live.se>
2026-01-29 16:39:14 +0100

    nu tar vi den

--- src/app/exhibitions/[slug]/page.tsx
+++ src/app/exhibitions/[slug]/page.tsx
@@ -1,12 +1,12 @@
+// src/app/exhibitions/[slug]/page.tsx
+
 import ExhibitionSlugModalClient from "@/app/exhibitions/ExhibitionSlugModalClient";
 
-interface PageProps {
-  params: {
-    slug: string;
-  };
-}
-
-export default function ExhibitionPage({ params }: PageProps) {
+export default function ExhibitionPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
   return (
     <div className="min-h-screen grid grid-cols-6 max-w-7xl">
       <ExhibitionSlugModalClient slug={params.slug} />
