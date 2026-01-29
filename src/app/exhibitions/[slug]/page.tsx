5fb5feddb707c9c57973eadded283a1fab49ea23
joeljarvi <joeljarvi@live.se>
2026-01-29 16:35:23 +0100

    såhär tror jag

--- src/app/exhibitions/[slug]/page.tsx
+++ src/app/exhibitions/[slug]/page.tsx
@@ -1,16 +1,12 @@
-"use client";
-
 import ExhibitionSlugModalClient from "@/app/exhibitions/ExhibitionSlugModalClient";
 
-type Params = {
-  slug: string;
-};
-
-interface Props {
-  params: Params;
+interface PageProps {
+  params: {
+    slug: string;
+  };
 }
 
-export default function ExhibitionPage({ params }: Props) {
+export default function ExhibitionPage({ params }: PageProps) {
   return (
     <div className="min-h-screen grid grid-cols-6 max-w-7xl">
       <ExhibitionSlugModalClient slug={params.slug} />
