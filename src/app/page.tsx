f8b191d4c4b92093d1933b817dec5d3e687cfd4b
joeljarvi <joeljarvi@live.se>
2026-01-29 16:11:55 +0100

    ok bort med loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import HomePageClient from "./HomePageClient";
 import React from "react";
-import { Loader } from "@/components/Loader";
 
 export default function Home() {
   return (
     <>
-      <React.Suspense fallback={<Loader />}>
-        <HomePageClient />
-      </React.Suspense>
+      <HomePageClient />
     </>
   );
 }
